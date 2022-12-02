774bd1a5ef8a5dccdbf1a0fce5f191bf259e7038
Larvet Khelian <khelian.larvet@etu.umontpellier.fr>
2022-12-02 09:33:01 +0100

    jpp

--- src/sampleapp.cpp
+++
@@ -1,9 +0,0 @@
-#include <sampleapp.h>
-
-SampleApp::SampleApp(int &argc, char *argv[]) :
-    QApplication(argc, argv)
-{
-    setApplicationName("Sample App");
-    setApplicationVersion("0.1");
-}
-
